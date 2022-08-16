64aa3b60fa0f4b5f685c469eedaee0bd49d5d997
Martynas Petruska <martynas.petruska@teliacompany.com>
2022-08-16 15:15:47 +0300

    feat: added storage component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,3 +1,5 @@
 export * from './Container';
 export * from './SongTile';
 export * from './SongsSwimlane';
+export * from './StorageListItem';
+export * from './StorageContainer';
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,18 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {ScrollView} from 'react-native';
 import {Text} from '../components/atoms';
-import {Container} from '../components/molecules';
+import {
+  Container,
+  StorageContainer,
+  StorageListItem,
+} from '../components/molecules';
 import {SongsContainer} from '../components/organisms';
+import {NavigationProps} from '../navigation';
 import {useGenresQuery} from '../state/services';
 
 export const HomeScreen = () => {
+  const {navigate} = useNavigation<NavigationProps>();
   const {data, error, isLoading} = useGenresQuery();
 
   if (isLoading) {
@@ -16,13 +23,32 @@ export const HomeScreen = () => {
     return <Text type="h2" text="Error" />;
   }
 
+  const navigateToMemoryScreen =
+    (screen: 'FileSystemScreen' | 'MemoryScreen') => () => {
+      navigate(screen);
+    };
+
   return (
     <Container>
       <ScrollView>
         {data.map((genre, idx) => (
           <SongsContainer key={idx} songGenre={genre} />
         ))}
-        <Text type="h1" text="Storage" />
+        <StorageContainer>
+          <Text type="h1" text="Storage" />
+          <StorageListItem
+            text="Memory"
+            // TODO: count the number of songs in the memory
+            songLength="1:00"
+            onPress={navigateToMemoryScreen('MemoryScreen')}
+          />
+          <StorageListItem
+            text="Filesystem"
+            // TODO: count the number of songs in the filesystem
+            songLength="1:00"
+            onPress={navigateToMemoryScreen('FileSystemScreen')}
+          />
+        </StorageContainer>
       </ScrollView>
     </Container>
   );
